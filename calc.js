3262dc73a215b22c61356239b0f3b13114e3bf13
aidenwalker42 <khoul.pixel@gmail.com>
2021-05-24 17:24:28 -0500

    commit

--- calc.js
+++ calc.js
@@ -7,12 +7,43 @@ const reader = readline.createInterface({
 
 
 
-reader.question("What would you like to calculate?", function(input){
-	tokens = input.split(' ');
+reader.question("What would you like to calculate? ", function(input){
+	tokens = input.split(' '); //allows for splitting
 	
 	mathSymbol = tokens[0];
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
+
+	// mathSymbol = "+"
+	// num1 = 3
+	// num2 = 4
+
+	console.log("Operand: ",mathSymbol); //prints out the text "Operand: " and the variable mathSymbol.
+	console.log("First Number ",num1);
+	console.log("Second Number ",num2);
+
+	if(mathSymbol === "add")
+	{
+		console.log(num1 + "+" + num2 + "=" + parseFloat(num1+num2)); //
+	}
+	if(mathSymbol=== "sub")
+	{
+		console.log(num1 + "+" + num2 + "=" + parseFloat(num1-num2));
+	}
+	if(mathSymbol=== "div")
+	{
+		console.log(num1 + "/" + num2 + "=" + parseFloat(num1/num2));
+	}
+	if(mathSymbol=== "multi")
+	{
+		console.log(num1 + "*" + num2 + "=" + parseFloat(num1*num2));
+	}
+	if(mathSymbol=== "root")
+	{
+		console.log("Square root = " + Math.sqrt(parseFloat(num1*num2)));
+	}
+
+
 
 
 	// This line closes the connection to the command line interface.
